3ebaacdab5dd8f7ff1f43e6756d54a970f7d9215
Ranga <rangakoundinya@gmail.com>
2017-09-11 22:41:23 +0200

    First commit

---
+++ src/hello/HelloWorld.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hello;
+
+/**
+ *
+ * @author ranga
+ */
+public class HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Greeter greeter = new Greeter();
+        System.out.println(greeter.sayHello());
+    }
+    
+}
